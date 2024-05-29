a1a4ebec78f15cb49f9da29fd4d7674c0d4af255
Shreyas Aradhya <60729435+ShreyasBlr@users.noreply.github.com>
2024-05-29 19:52:05 +0530

    Render Deployment - Staging

--- src/config-global.js
+++ src/config-global.js
@@ -1,2 +1,3 @@
 export const PATH_AFTER_LOGIN = "/dashboard";
-export const HOST_API = "http://localhost:5200";
+// export const HOST_API = "http://localhost:5200";
+export const HOST_API = "https://savvy-backend-vklg.onrender.com";
