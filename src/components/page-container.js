6ce303325a62ce6b55f0c5f5b090a455c03a909a
Shreyas Aradhya <60729435+ShreyasBlr@users.noreply.github.com>
2024-06-02 21:51:29 +0530

    Removed scrollbar inside page-container

--- src/components/page-container.js
+++ src/components/page-container.js
@@ -1,6 +1,6 @@
 const PageConatiner = ({ children }) => {
   return (
-    <div className="flex flex-col gap-3 h-[calc(100dvh-160px)] pb-4 overflow-hidden overflow-y-scroll">
+    <div className="flex flex-col gap-3 h-[calc(100dvh-160px)] pb-4 overflow-hidden overflow-y-scroll no-scrollbar">
       {children}
     </div>
   );
--- src/sections/dashboard/recent-transations.js
+++ src/sections/dashboard/recent-transations.js
@@ -2,6 +2,7 @@ import transactions from "@/data/transactions";
 import { Button } from "@/components/ui/button";
 import Iconify from "@/components/iconify";
 import TransactionCard from "../common/transaction-card";
+import Link from "next/link";
 
 const RecentTransactions = () => {
   const recentTransactions = transactions.slice(0, 4);
@@ -11,13 +12,15 @@ const RecentTransactions = () => {
         <h5 className="text-slate-200 text-md font-semibold">
           Recent transactions
         </h5>
-        <Button variant="ghost" className="text-slate-200">
-          View all
-          <Iconify
-            icon="solar:alt-arrow-right-linear"
-            className="ml-2 w-4 h-4"
-          />
-        </Button>
+        <Link href="/dashboard/transactions">
+          <Button variant="ghost" className="text-slate-200">
+            View all
+            <Iconify
+              icon="solar:alt-arrow-right-linear"
+              className="ml-2 w-4 h-4"
+            />
+          </Button>
+        </Link>
       </div>
       {recentTransactions.map((transaction) => (
         <TransactionCard key={transaction._id} transaction={transaction} />
