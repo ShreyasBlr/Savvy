f07f5b1db86932a173c805eabc65c1c9096552e0
Shreyas Aradhya <60729435+ShreyasBlr@users.noreply.github.com>
2024-06-03 23:33:16 +0530

    New UI for add transaction

--- src/layouts/dashboard/add-transaction.js
+++ src/layouts/dashboard/add-transaction.js
@@ -4,18 +4,22 @@ import Iconify from "@/components/iconify";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
 
 import useTheme from "@/hooks/useTheme";
-import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export function AddTransaction() {
   const theme = useTheme();
-
+  const router = useRouter();
   return (
     <Dialog className="max-w-lg">
       <DialogTrigger asChild>
@@ -28,12 +32,36 @@ export function AddTransaction() {
         </Button>
       </DialogTrigger>
       <DialogContent className="max-w-[325px]">
-        <DialogHeader>
+        <DialogHeader className="mb-4">
           <DialogTitle>Add Transaction</DialogTitle>
         </DialogHeader>
-        <h2>Income</h2>
-        <h2>or</h2>
-        <h2>Expense</h2>
+        <DialogClose asChild>
+          <button onClick={() => router.push("/dashboard/transactions")}>
+            <div className="flex flex-col gap-4 justify-center items-center">
+              <Image
+                src={"/illustrations/income.png"}
+                alt="logo"
+                width={80}
+                height={80}
+              />
+              <h2 className="text-center">Income</h2>
+            </div>
+          </button>
+        </DialogClose>
+        <Separator className="my-4" />
+        <DialogClose asChild>
+          <button onClick={() => router.push("/dashboard/transactions")}>
+            <div className="flex flex-col gap-4 justify-center items-center">
+              <Image
+                src={"/illustrations/expense.png"}
+                alt="logo"
+                width={80}
+                height={80}
+              />
+              <h2>Expense</h2>
+            </div>
+          </button>
+        </DialogClose>
       </DialogContent>
     </Dialog>
   );
