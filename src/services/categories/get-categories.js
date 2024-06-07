e2d744656e6ac080fa529ffd1c6a16c2a79696a9
Shreyas Aradhya <60729435+ShreyasBlr@users.noreply.github.com>
2024-06-08 02:01:38 +0530

    Update: Axios bug on server

--- src/services/categories/get-categories.js
+++ src/services/categories/get-categories.js
@@ -4,12 +4,9 @@ const getCategories = async (type) => {
   const income = Boolean(type?.income);
   const expense = Boolean(type?.expense);
   try {
-    const response = await axiosInstance.get(endpoints.categories.root, {
-      params: {
-        income,
-        expense,
-      },
-    });
+    const response = await axiosInstance.get(
+      `${endpoints.categories.root}?income=${income}&expense=${expense}`
+    );
     return response.data;
   } catch (error) {
     throw new Error(error);
