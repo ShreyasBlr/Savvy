90d317d7cf37336057347ed62559dc87951d32ec
Shreyas Aradhya <60729435+ShreyasBlr@users.noreply.github.com>
2024-06-09 11:37:03 +0530

    Update: removed next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// import withPWA from "next-pwa";
-
-// const pwa = withPWA({
-//   dest: "public",
-//   sw: "service-worker.js",
-// });
-
-// export default pwa;
